58bad2f771eaa71a293294ebaa8f11458821a8d2
Niko Triantafillou <ec1632074@edinburghcollege.ac.uk>
2018-11-29 07:40:52 +0000

    testing views

--- source/views.py
+++ source/views.py
@@ -4,6 +4,32 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 from . import models
+from flask import Flask, g
+import sqlite3
+
+from flask import Flask
+app = Flask(__name__)
+db_location = 'var/sqlite3.db'
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = sqlite3.connect(db_location)
+        g.db = db
+    return db
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.route('/')
 @app.route('/index')
