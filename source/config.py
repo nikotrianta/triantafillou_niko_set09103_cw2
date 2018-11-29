461fa6cd2f991f823f6b675ed2bf775edac4e2e4
Niko Triantafillou <ec1632074@edinburghcollege.ac.uk>
2018-11-29 04:33:03 +0000

    updating config

--- source/config.py
+++ source/config.py
@@ -2,7 +2,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
 
 class Config(object):
     # ...
