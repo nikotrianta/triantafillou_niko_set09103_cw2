d75a03de7bd3b28569ace796e739a06b245ff527
Niko Triantafillou <ec1632074@edinburghcollege.ac.uk>
2018-11-29 15:51:44 +0000

    updating config

--- source/config.py
+++ source/config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('var/sqlite3.db') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('/static/var/sqlite3.db') or \
         'sqlite:///' + os.path.join(basedir, 'static/var/sqlite3.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
