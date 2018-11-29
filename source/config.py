8b49c2075d9e4b2b95b2150f0b7d18034cdb5c82
Niko Triantafillou <ec1632074@edinburghcollege.ac.uk>
2018-11-29 08:44:13 +0000

    fixing errors

--- source/config.py
+++ source/config.py
@@ -2,4 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
