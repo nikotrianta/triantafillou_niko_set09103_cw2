e7eb46eb877ae432c3ce039ef33cd311909edd56
Niko Triantafillou <ec1632074@edinburghcollege.ac.uk>
2018-11-29 07:31:03 +0000

    adding schema and modifying models

--- source/models.py
+++ source/models.py
@@ -1,11 +1,41 @@
-from datetime import datetime
-from . import db
+from flask import Flask, g
+import sqlite3
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(140))
-    message = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+from flask import Flask
+app = Flask(__name__)
+db_location = 'var/sqlite3.db'
 
-    def __repr__(self):
-        return '<Post {}>'.format(self.message)
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = sqlite3.connect(db_location)
+        g.db = db
+    return db
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def init.db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+	form = PostForm()
+	if form.validate_on_submit():
+        user = 'AnonUser'
+		# User(form.user.data) if user is not None else "Anon"
+		message = 'Test message'
+		# Message(form.message.data)
+        db = get_db()
+        db.cursor().execute('insert into post values ("'user'", "'message'")')
+        db.commit()
+		flash('Thanks for posting!')
+		return redirect('/index')
+	return render_template('post.html', form=form)
---
+++ source/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE if EXISTS post;
+
+CREATE TABLE post (
+    id int,
+    user text,
+    message text
+)
