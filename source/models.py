62f83c749ea908fa9f60f9fe6fa93c0de8ff80b4
Niko Triantafillou <ec1632074@edinburghcollege.ac.uk>
2018-11-29 05:04:01 +0000

    updating models

--- source/models.py
+++ source/models.py
@@ -1,17 +1,10 @@
 from datetime import datetime
 from . import db
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(64), index=True, unique=True)
-    message = db.relationship('Message', backref='author', lazy='dynamic')
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
+    user = db.Column(db.String(140))
+    message = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
